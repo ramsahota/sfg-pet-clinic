f06721f1a2539cd772b60caf9930e86fc78f3d5a
ronsahota <ronsahota@hotmail.com>
2020-03-22 15:15:51 -0400

    adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -6,6 +6,10 @@ public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
