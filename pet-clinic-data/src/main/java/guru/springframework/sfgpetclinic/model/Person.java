c0b6298b8bace09f8cd38d44c34f913412f10441
ronsahota <ronsahota@hotmail.com>
2020-04-12 19:40:25 -0400

    Added Owners List and updated controller. closes #16

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -6,6 +6,10 @@ public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
